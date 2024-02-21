2975b0d367f307810fca83da96b596fc3c646692
Sam Lu <pbcmarsh@gmail.com>
2024-02-21 15:22:28 -0600

    is subsequence java solution

---
+++ Two Pointers/392-Is-Subsequence-1.java
@@ -0,0 +1,20 @@
+/**
+ * Approach: Loop through string and compare
+ * n = length of string t
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int index = 0;
+        for (int i = 0; i < t.length(); i++) {
+            if (index == s.length()) {
+                return true;
+            }
+            if (s.charAt(index) == t.charAt(i)) {
+                index++;                
+            }
+        }
+        return index == s.length();
+    }
+}
